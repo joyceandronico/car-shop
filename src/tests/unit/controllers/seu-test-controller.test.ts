433784abfdcc5149484081288bd374d1397e6908
joyceandronicco <joyceandronico@msn.com>
2022-11-02 02:51:41 -0300

    test: :white_check_mark: teste para a camada Controller

--- src/tests/unit/controllers/seu-test-controller.test.ts
+++ src/tests/unit/controllers/seu-test-controller.test.ts
@@ -1,22 +1,42 @@
-// template para criação dos testes de cobertura da camada de controller
+import * as sinon from 'sinon';
+import { expect } from 'chai';
+import { CarMockTest } from '../../mocks/carMock';
+import { Request, Response } from 'express';
+import CarController from '../../../controllers/car.controller';
+import CarService from '../../../services/car.service';
+import CarModel from '../../../models/car.model';
 
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+describe('carController', () => {
+    const carModel = new CarModel();
+    const carService = new CarService(carModel);
+    const carController = new CarController(carService);
 
-// describe('Sua descrição', () => {
+    const req = {} as Request;
+    const res = {} as Response;
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  before(() => {
+    sinon.stub(carService, 'create').resolves(CarMockTest);
 
-//   it('', async () => {});
 
-// });
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('create', () => {
+  it('Carro cadastrado com sucesso', async () => {
+    req.body = CarMockTest;
+    await carController.create(req, res);
+
+    expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+    expect((res.json as sinon.SinonStub).calledWith(CarMockTest)).to.be.true;
+  });
+
+ });
+
+});
